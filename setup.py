5d62fc8ed39308d81a63de279a44710e243d7338
Aitzol Naberan Burgaña <anaberan@codesyntax.com>
2021-07-28 10:50:29 +0200

    Set version number

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ long_description = "\n\n".join(
 
 setup(
     name="clms.downloadtool",
-    version="1.0a1",
+    version="1.0.1",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
