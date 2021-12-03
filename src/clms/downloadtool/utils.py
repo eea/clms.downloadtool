16dcdb5e7f9bba23a81202fdda9f0cb7ffaaeb9a
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-12-03 12:42:17 +0100

    lint

--- src/clms/downloadtool/utils.py
+++ src/clms/downloadtool/utils.py
@@ -1,4 +1,7 @@
 """ export some constants here too"""
 # pylint: disable-line-too-long
+# pylint: disable-unused-import
 from clms.downloadtool.api.services.utils.constants import DATASET_FORMATS  # noqa
+# pylint: disable-line-too-long
+# pylint: disable-unused-import
 from clms.downloadtool.api.services.utils.constants import DATASET_SOURCES  # noqa
