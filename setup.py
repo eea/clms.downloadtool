35604b29a22c3e324e3277877cf26cd7ae25b825
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-12-03 14:10:24 +0100

    fix

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
             # plone_coredev tests as of 2016-04-01.
             "plone.testing>=5.0.0",
             "plone.app.contenttypes",
+            "plone.app.robotframework",
         ],
     },
     entry_points="""
