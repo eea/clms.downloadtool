bce01275a3f4e9390a966d9c022fe448ea2717fd
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-05-31 16:02:02 +0200

    lint

--- src/clms/downloadtool/api/services/datarequest_inspect/get.py
+++ src/clms/downloadtool/api/services/datarequest_inspect/get.py
@@ -5,7 +5,6 @@ For HTTP GET operations we can use standard HTTP parameter passing
 
 """
 from clms.downloadtool.utility import IDownloadToolUtility
-from plone import api
 from plone.restapi.services import Service
 from zope.component import getUtility
 
@@ -27,14 +26,6 @@ class DatarequestInspect(Service):
                 query[available_filter] = self.request.get(available_filter)
 
         response_json = utility.datarequest_inspect(**query)
-
-        if "Error, UserID not defined" in response_json:
-            self.request.response.setStatus(400)
-            return {"status": "error", "msg": response_json}
-
-        if "Error, status not recognized" in response_json:
-            self.request.response.setStatus(400)
-            return {"status": "error", "msg": response_json}
 
         self.request.response.setStatus(200)
         return response_json
