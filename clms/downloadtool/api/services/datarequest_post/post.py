2317a65f3f8872a32d882d06f75254e8151f896d
ana-oprea <anaop3212@gmail.com>
2024-10-09 15:39:11 +0300

    feat: mandatory area for non-EEA datasets - refs #278003

--- clms/downloadtool/api/services/datarequest_post/post.py
+++ clms/downloadtool/api/services/datarequest_post/post.py
@@ -505,6 +505,23 @@ class DataRequestPost(Service):
                 elif is_special_case:
                     continue
 
+                if ("NUTS" not in dataset_json and "BoundingBox" not in dataset_json):  # noqa
+                    # We are requesting a full dataset download
+                    # We need to check if this dataset is a non-EEA dataset
+                    # to show a specific message
+                    # pylint: disable=line-too-long
+                    if full_dataset_source and full_dataset_source != "EEA":
+                        print('full_dataset_source', full_dataset_source)
+                        # Non-EEA datasets must have an area specified
+                        self.request.response.setStatus(400)
+                        return {
+                            "status": "error",
+                            "msg": (
+                                "You have to select a specific area of interest. "
+                                "In case you want to download the full dataset, please use the Auxiliary API."
+                            ),
+                        }
+
                 # Check full dataset download restrictions
                 # pylint: disable=line-too-long
                 if ("NUTS" not in dataset_json and "BoundingBox" not in dataset_json and "TemporalFilter" not in dataset_json):  # noqa
