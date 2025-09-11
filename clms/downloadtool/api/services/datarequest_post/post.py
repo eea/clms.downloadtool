a27352cd8ef2b3d2d1de44a344a1b723807e712d
GhitaB <ghitabizau27@gmail.com>
2025-09-11 08:53:40 +0300

    Refs #291977 - WIP refactor datarequest_post (1115 -> 954).

--- clms/downloadtool/api/services/datarequest_post/post.py
+++ clms/downloadtool/api/services/datarequest_post/post.py
@@ -7,7 +7,6 @@ through the URL)
 import copy
 import base64
 import json
-import re
 import uuid
 from datetime import datetime
 from datetime import timedelta
@@ -16,11 +15,10 @@ from logging import getLogger
 
 import requests
 from clms.downloadtool.api.services.utils import (
-    get_extra_data,
     duplicated_values_exist,
 )
 from clms.downloadtool.utility import IDownloadToolUtility
-from clms.downloadtool.utils import COUNTRIES, FORMAT_CONVERSION_TABLE
+from clms.downloadtool.utils import FORMAT_CONVERSION_TABLE
 from clms.downloadtool.api.services.utils import (
     calculate_bounding_box_area,
     get_available_gcs_values,
@@ -28,9 +26,23 @@ from clms.downloadtool.api.services.utils import (
 from clms.downloadtool.api.services.cdse.cdse_integration import (
     create_batch, start_batch)
 from clms.downloadtool.api.services.datarequest_post.utils import (
-    to_iso8601, generate_task_group_id)
+    ISO8601_DATETIME_FORMAT,
+    to_iso8601,
+    generate_task_group_id,
+    extract_dates_from_temporal_filter,
+    validate_spatial_extent,
+    validate_nuts,
+    get_task_id,
+    save_stats,
+    get_dataset_file_path_from_file_id,
+    get_dataset_file_source_from_file_id,
+    get_full_dataset_format,
+    get_full_dataset_source,
+    get_full_dataset_path,
+    get_full_dataset_wekeo_choices,
+    get_full_dataset_layers,
+)
 
-from clms.statstool.utility import IDownloadStatsUtility
 from plone import api
 from plone.memoize.ram import cache
 from plone.memoize.view import memoize
@@ -39,9 +51,6 @@ from plone.restapi.deserializer import json_body
 from plone.restapi.services import Service
 from zope.component import getUtility
 from zope.interface import alsoProvides
-
-
-ISO8601_DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
 def _cache_key(fun, self, nutsid):
@@ -943,173 +952,3 @@ class DataRequestPost(Service):
                         return nuts_name
 
         return nutsid
-
-
-def extract_dates_from_temporal_filter(temporal_filter):
-    """StartDate and EndDate are mandatory and come in miliseconds since
-    epoch, so we need to convert them to datetime objects first and to
-    ISO8601-like format then.
-    """
-    try:
-        start_date = temporal_filter.get("StartDate")
-        end_date = temporal_filter.get("EndDate")
-
-        start_date_obj = datetime.fromtimestamp(start_date / 1000)
-        end_date_obj = datetime.fromtimestamp(end_date / 1000)
-
-        return (
-            start_date_obj.strftime(ISO8601_DATETIME_FORMAT),
-            end_date_obj.strftime(ISO8601_DATETIME_FORMAT),
-        )
-    except (TypeError, ValueError):
-        return None, None
-
-
-def validate_spatial_extent(bounding_box):
-    """validate Bounding Box"""
-    if not len(bounding_box) == 4:
-        return False
-
-    for x in bounding_box:
-        if not isinstance(x, int) and not isinstance(x, float):
-            return False
-
-    return True
-
-
-def validate_nuts(nuts_id):
-    """validate nuts"""
-    if not nuts_id.isalnum():
-        return False
-
-    match = re.match(r"([A-Z]+)([0-9]*)", nuts_id, re.I)
-    if match:
-        items = match.groups()
-        # Only the first 2 chars represent the country
-        # french NUTS codes have 3 alphanumeric chars and then numbers
-        valid_nuts = items[0][:2] in COUNTRIES.keys()
-        return valid_nuts
-    return None
-
-
-def get_task_id(params):
-    """GetTaskID Method"""
-    for item in params:
-        return item
-
-
-def save_stats(stats_json):
-    """save the stats in the download stats utility"""
-    try:
-        utility = getUtility(IDownloadStatsUtility)
-        stats_json.update(get_extra_data(stats_json))
-        utility.register_item(stats_json)
-    except Exception as e:
-        log.exception(e)
-        log.info(
-            "There was an error saving the stats: %s", json.dumps(stats_json)
-        )  # noqa
-
-
-def get_dataset_file_path_from_file_id(dataset_object, file_id):
-    """get the dataset file path from the file id"""
-    downloadable_files_json = dataset_object.downloadable_files
-    for file_object in downloadable_files_json.get("items", []):
-        if file_object.get("@id") == file_id:
-            return file_object.get("path", "")
-
-    return None
-
-
-def get_dataset_file_source_from_file_id(dataset_object, file_id):
-    """get the dataset file format from the file id"""
-    downloadable_files_json = dataset_object.downloadable_files
-    for file_object in downloadable_files_json.get("items", []):
-        if file_object.get("@id") == file_id:
-            return file_object.get("source", "")
-
-    return None
-
-
-def get_full_dataset_format(dataset_object, download_information_id):
-    """get the dataset full format based on the requested
-    download_information_id"""
-    dataset_download_information_json = (
-        dataset_object.dataset_download_information
-    )
-    for download_information in dataset_download_information_json.get(
-        "items", []
-    ):
-        if download_information.get("@id") == download_information_id:
-            value = download_information.get("full_format", "")
-            if isinstance(value, dict):
-                return value.get("token", "")
-
-            return value
-
-    return None
-
-
-def get_full_dataset_source(dataset_object, download_information_id):
-    """get the dataset full source based on the requested
-    download_information_id"""
-    dataset_download_information_json = (
-        dataset_object.dataset_download_information
-    )
-    for download_information in dataset_download_information_json.get(
-        "items", []
-    ):
-        if download_information.get("@id") == download_information_id:
-            value = download_information.get("full_source", "")
-            if isinstance(value, dict):
-                return value.get("token", "")
-
-            return value
-
-    return None
-
-
-def get_full_dataset_path(dataset_object, download_information_id):
-    """get the dataset full path based on the requested
-    download_information_id"""
-    dataset_download_information_json = (
-        dataset_object.dataset_download_information
-    )
-    for download_information in dataset_download_information_json.get(
-        "items", []
-    ):
-        if download_information.get("@id") == download_information_id:
-            return download_information.get("full_path", "")
-
-    return None
-
-
-def get_full_dataset_wekeo_choices(dataset_object, download_information_id):
-    """get the dataset wekeo_choices based on the requested
-    download_information_id"""
-    dataset_download_information_json = (
-        dataset_object.dataset_download_information
-    )
-    for download_information in dataset_download_information_json.get(
-        "items", []
-    ):
-        if download_information.get("@id") == download_information_id:
-            return download_information.get("wekeo_choices", "")
-
-    return None
-
-
-def get_full_dataset_layers(dataset_object, download_information_id):
-    """get the available layers/bands based on the requested
-    download_information_id
-    """
-    dataset_download_information_json = (
-        dataset_object.dataset_download_information
-    )
-    for download_information in dataset_download_information_json.get(
-        "items", []
-    ):
-        if download_information.get("@id") == download_information_id:
-            return download_information.get("layers", [])
-
-    return []
--- clms/downloadtool/api/services/datarequest_post/utils.py
+++ clms/downloadtool/api/services/datarequest_post/utils.py
@@ -1,4 +1,17 @@
 """Utils"""
+import random
+import re
+import json
+from logging import getLogger
+from datetime import datetime
+from zope.component import getUtility
+from clms.downloadtool.utils import COUNTRIES
+from clms.downloadtool.api.services.utils import get_extra_data
+from clms.statstool.utility import IDownloadStatsUtility
+
+
+ISO8601_DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+log = getLogger(__name__)
 
 
 def to_iso8601(dt_str):
@@ -16,3 +29,173 @@ def generate_task_group_id():
         group = ''.join(str(random.randint(0, 9)) for _ in range(4))
         groups.append(group)
     return '-'.join(groups)
+
+
+def extract_dates_from_temporal_filter(temporal_filter):
+    """StartDate and EndDate are mandatory and come in miliseconds since
+    epoch, so we need to convert them to datetime objects first and to
+    ISO8601-like format then.
+    """
+    try:
+        start_date = temporal_filter.get("StartDate")
+        end_date = temporal_filter.get("EndDate")
+
+        start_date_obj = datetime.fromtimestamp(start_date / 1000)
+        end_date_obj = datetime.fromtimestamp(end_date / 1000)
+
+        return (
+            start_date_obj.strftime(ISO8601_DATETIME_FORMAT),
+            end_date_obj.strftime(ISO8601_DATETIME_FORMAT),
+        )
+    except (TypeError, ValueError):
+        return None, None
+
+
+def validate_spatial_extent(bounding_box):
+    """validate Bounding Box"""
+    if not len(bounding_box) == 4:
+        return False
+
+    for x in bounding_box:
+        if not isinstance(x, int) and not isinstance(x, float):
+            return False
+
+    return True
+
+
+def validate_nuts(nuts_id):
+    """validate nuts"""
+    if not nuts_id.isalnum():
+        return False
+
+    match = re.match(r"([A-Z]+)([0-9]*)", nuts_id, re.I)
+    if match:
+        items = match.groups()
+        # Only the first 2 chars represent the country
+        # french NUTS codes have 3 alphanumeric chars and then numbers
+        valid_nuts = items[0][:2] in COUNTRIES.keys()
+        return valid_nuts
+    return None
+
+
+def get_task_id(params):
+    """GetTaskID Method"""
+    for item in params:
+        return item
+
+
+def save_stats(stats_json):
+    """save the stats in the download stats utility"""
+    try:
+        utility = getUtility(IDownloadStatsUtility)
+        stats_json.update(get_extra_data(stats_json))
+        utility.register_item(stats_json)
+    except Exception as e:
+        log.exception(e)
+        log.info(
+            "There was an error saving the stats: %s", json.dumps(stats_json)
+        )  # noqa
+
+
+def get_dataset_file_path_from_file_id(dataset_object, file_id):
+    """get the dataset file path from the file id"""
+    downloadable_files_json = dataset_object.downloadable_files
+    for file_object in downloadable_files_json.get("items", []):
+        if file_object.get("@id") == file_id:
+            return file_object.get("path", "")
+
+    return None
+
+
+def get_dataset_file_source_from_file_id(dataset_object, file_id):
+    """get the dataset file format from the file id"""
+    downloadable_files_json = dataset_object.downloadable_files
+    for file_object in downloadable_files_json.get("items", []):
+        if file_object.get("@id") == file_id:
+            return file_object.get("source", "")
+
+    return None
+
+
+def get_full_dataset_format(dataset_object, download_information_id):
+    """get the dataset full format based on the requested
+    download_information_id"""
+    dataset_download_information_json = (
+        dataset_object.dataset_download_information
+    )
+    for download_information in dataset_download_information_json.get(
+        "items", []
+    ):
+        if download_information.get("@id") == download_information_id:
+            value = download_information.get("full_format", "")
+            if isinstance(value, dict):
+                return value.get("token", "")
+
+            return value
+
+    return None
+
+
+def get_full_dataset_source(dataset_object, download_information_id):
+    """get the dataset full source based on the requested
+    download_information_id"""
+    dataset_download_information_json = (
+        dataset_object.dataset_download_information
+    )
+    for download_information in dataset_download_information_json.get(
+        "items", []
+    ):
+        if download_information.get("@id") == download_information_id:
+            value = download_information.get("full_source", "")
+            if isinstance(value, dict):
+                return value.get("token", "")
+
+            return value
+
+    return None
+
+
+def get_full_dataset_path(dataset_object, download_information_id):
+    """get the dataset full path based on the requested
+    download_information_id"""
+    dataset_download_information_json = (
+        dataset_object.dataset_download_information
+    )
+    for download_information in dataset_download_information_json.get(
+        "items", []
+    ):
+        if download_information.get("@id") == download_information_id:
+            return download_information.get("full_path", "")
+
+    return None
+
+
+def get_full_dataset_wekeo_choices(dataset_object, download_information_id):
+    """get the dataset wekeo_choices based on the requested
+    download_information_id"""
+    dataset_download_information_json = (
+        dataset_object.dataset_download_information
+    )
+    for download_information in dataset_download_information_json.get(
+        "items", []
+    ):
+        if download_information.get("@id") == download_information_id:
+            return download_information.get("wekeo_choices", "")
+
+    return None
+
+
+def get_full_dataset_layers(dataset_object, download_information_id):
+    """get the available layers/bands based on the requested
+    download_information_id
+    """
+    dataset_download_information_json = (
+        dataset_object.dataset_download_information
+    )
+    for download_information in dataset_download_information_json.get(
+        "items", []
+    ):
+        if download_information.get("@id") == download_information_id:
+            return download_information.get("layers", [])
+
+    return []
