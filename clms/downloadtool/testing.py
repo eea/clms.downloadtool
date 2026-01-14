d9e61b304dd0f99f5bfc26552fcdfacfc4e01d22
GhitaB <ghitabizau27@gmail.com>
2026-01-14 16:48:42 +0200

    Refs #296456 - DownloadTool use Postgres - update testing.

--- clms/downloadtool/testing.py
+++ clms/downloadtool/testing.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 """Test plone site."""
 import os
+
 import clms.downloadtool
 import plone.restapi
 from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
-from plone.app.testing import (FunctionalTesting, IntegrationTesting,
-                               PloneSandboxLayer, applyProfile)
+from plone.app.testing import (
+    FunctionalTesting,
+    IntegrationTesting,
+    PloneSandboxLayer,
+    applyProfile,
+)
 from plone.testing.zope import WSGI_SERVER_FIXTURE
+from zope.component import getUtility
+
+from clms.downloadtool.storage.memory import MemoryDownloadtoolRepository
+from clms.downloadtool.utility import IDownloadToolUtility
 
 
 class ClmsDownloadtoolLayer(PloneSandboxLayer):
@@ -27,6 +36,16 @@ class ClmsDownloadtoolLayer(PloneSandboxLayer):
     def setUpPloneSite(self, portal):
         """ Setup cms site """
         applyProfile(portal, "clms.downloadtool:default")
+
+    def testSetUp(self):
+        """Reset in-memory download task storage between tests."""
+        os.environ.setdefault("CLMS_DOWNLOADTOOL_TESTING", "1")
+        utility = getUtility(IDownloadToolUtility)
+        repository = getattr(utility, "_repository", None)
+        if isinstance(repository, MemoryDownloadtoolRepository):
+            repository.delete_all()
+        else:
+            utility._repository = MemoryDownloadtoolRepository()
 
 
 CLMS_DOWNLOADTOOL_FIXTURE = ClmsDownloadtoolLayer()
